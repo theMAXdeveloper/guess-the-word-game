00c06becde9e500e6a2b5a8d60d629d378efdd7b
max_g <maxgongob@gmail.com>
2025-04-09 16:30:07 -1000

    Created global variables, added letter placeholders, added button event

---
+++ js/script.js
@@ -0,0 +1,44 @@
+// <UL> where guessed letters appear //
+const guessedLetters = document.querySelector(".guessed-letters");
+
+// Guess Button //
+const guessButton = document.querySelector("button.guess");
+
+// Text input for guessing letter //
+const letterInput = document.querySelector("input#letter");
+
+// Empty <p> where word in progress will appear //
+const wordInProgress = document.querySelector(".word-in-progress");
+
+// <p> for remaining guesses //
+const remaining = document.querySelector(".remaining");
+
+// remaining guesses <span> //
+const reamainingSpan = document.querySelector(".remaining span");
+
+// empty <p> where messages will appear after guessing letter //
+const message = document.querySelector(".message");
+
+// hidden button to prompt player to play again //
+const playAgainButton = document.querySelector(".play-again");
+
+// Temporary placeholder word //
+const word = "magnolia";
+
+
+const placeholder = function (word) {
+	const placeholderArray = []
+	for (const letter of word) {
+		placeholderArray.push("●");
+	}
+	wordInProgress.innerText = placeholderArray.join("");
+};
+
+placeholder(word);
+
+guessButton.addEventListener("click", function (e) {
+	e.preventDefault();
+	const inputValue = letterInput.value
+	console.log(inputValue);
+	letterInput.value = "";
+}); 
